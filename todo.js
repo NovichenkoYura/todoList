bd0d4282824607dadb168fb687a1b7c2177994da
NovichenkoYura <novichenko.y@gmail.com>
2022-07-13 18:12:14 +0300

    doesnt work footerBtn

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const addTaskBtn = document.getElementById('add-task-btn');
 const deskTaskInput = document.getElementById('desription-task');
 const todosWrapper = document.querySelector('.todos-wrapper');
+const footerBtnWrap = document.querySelector('.footer-wrap')
 
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -12,7 +13,7 @@ function Task(description) {
 	this.completed = false;
 }
 
-const createTemplate = (task, index) => {
+const createTemplateTasks = (task, index) => {
 	return `
 		<div class="todo-item ${task.completed ? 'checked' : ''}">
 			<div class="description">${task.description}</div>
@@ -28,8 +29,8 @@ const fillHtmlList = () => {
 	todosWrapper.innerHTML = "";
 	if (tasks.length > 0) {
 		tasks.forEach((item, index) => {
-			todosWrapper.innerHTML += createTemplate(item, index);
-			
+			todosWrapper.innerHTML += createTemplateTasks(item, index);
+						
 		});
 		todoItemsElems = document.querySelectorAll('.todo-item');
 	}
@@ -58,6 +59,7 @@ addTaskBtn.addEventListener('click', () => {
 	updateLocal();
 	fillHtmlList();
 	deskTaskInput.value = '';
+	renderFooterBtn();
 	
 })
 
@@ -70,4 +72,30 @@ const deleteTask = index => {
 	fillHtmlList();
 		
 	}, 500)
+}
+
+// const activeTask = tasks.filter(item => item.completed === false);
+// const completedTask = tasks.filter(item => item.completed === true);
+// console.log(activeTask)
+// console.log(completedTask)
+
+
+const createTemplateFooterBtn = (tasks) => {
+	const activeTask = tasks.filter(item => item.completed === false);
+	const completedTask = tasks.filter(item => item.completed === true);
+	console.log(activeTask.length)
+	console.log(completedTask)
+	return `
+		<p class="items-left">${activeTask.length}items left</p>
+		<button id="all-tasks">All</button>
+		<button id="active-tasks">Active</button>
+		<button id="completed-tasks">Completed</button>
+		<button id="clear-completed-tasks">Clear completed</button>
+
+	`
+}
+
+const renderFooterBtn = (tasks) => {
+	footerBtnWrap.innerHTML = "";
+	footerBtnWrap.innerHTML = createTemplateFooterBtn(tasks)
 }
